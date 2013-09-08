32ca37fdf0ce3d49c0140246361145d27212de61
Fumihisa Yamanobe <addy0920@gmail.com>
2013-09-08 15:20:26 +0900

    blog index added

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,25 @@
 class BlogsController < ApplicationController
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 RailsTest2::Application.routes.draw do
+
+  root 'blogs#index'
+  resources :blogs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
