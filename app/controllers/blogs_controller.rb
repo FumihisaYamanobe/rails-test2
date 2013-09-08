2d868643f198ad24d1415f20accff9211a3e141a
Fumihisa Yamanobe <addy0920@gmail.com>
2013-09-08 15:53:31 +0900

    new create added

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,21 +5,36 @@ class BlogsController < ApplicationController
   end
 
   def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to @blog
+    else
+      render 'new'
+    end
   end
 
   def new
+    @blog = Blog.new
   end
 
   def edit
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path
   end
 
+  private
+  def blog_params
+    params[:blog].permit(:titile, :contents, :tag)
+  end
 end
